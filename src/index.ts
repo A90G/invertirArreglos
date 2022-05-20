03f07f34fd339e7939e7ca764339ad42cf65b52c
A90G <104040612+A90G@users.noreply.github.com>
2022-05-20 20:09:57 -0300

    commit finalicè ejercicio

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 /*Almacene en un arreglo de tamaño N los números ingresados
 por el usuario
 La dimensión N también es ingresada por el usuario
-LO ÚNICO QUE ME FALTA ES QUE Muestre los números del arreglo pero del último al primero
+Muestre los números del arreglo pero del último al primero
 */
 let dimensionArreglo: number = Number(
   prompt(`Ingrese la dimensión del arreglo:`)
@@ -14,4 +14,9 @@ for (indice = 0; indice < dimensionArreglo; indice++) {
     prompt(`Indique el nro que va en la posición ${indice}:`)
   );
   console.log(`El número en la posición ${indice} es: ${arreglo[indice]}`);
+}
+for (indice = dimensionArreglo - 1; indice >= 0; indice--) {
+  console.log(
+    `Los números ingresados del último al primero son: posición ${indice} "${arreglo[indice]}"`
+  );
 }
